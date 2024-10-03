050f250eb66fa8fb2c0f8457683285803ed96e56
40ftslinky <jez@40ftslinky.com>
2024-10-03 10:21:27 +1000

    nav css / js changes

--- js/menu-active.js
+++ js/menu-active.js
@@ -1,13 +1,14 @@
 // menu-active.js
 
-// $("body").removeClass("no_scroll");
+// vanilla js - change class
 
 
 document.getElementById('menu-button').onclick = function() { 
     this.classList.toggle('active');
     document.getElementsByTagName('header')[0].classList.toggle('active');
-    document.getElementsByClassName('[class*="submenu"]')[0].classList.remove('open');
-    document.getElementsByClassName('wp-block-navigation-submenu')[0].classList.remove('open');
+    //document.getElementsByClassName('[class*="submenu"]')[0].classList.remove('open');
+	 document.getElementsByClassName('submenu')[0].classList.remove('open');
+    // document.getElementsByClassName('wp-block-navigation-submenu')[0].classList.remove('open');
     document.body.classList.toggle('no_scroll'); // Add 'no_scroll' class to body
 }
 
@@ -18,10 +19,10 @@ const menuLinks = document.getElementsByClassName('menu-link');
 for (let i = 0; i < menuLinks.length; i++) {
     menuLinks[i].onclick = function() {
         // Remove 'active' class from 'menu-button' and 'header' when any menu link except has-child is clicked
-        if (!this.classList.contains('has-child')) {
-            document.getElementById('menu-button').classList.remove('active');
-            document.getElementById('header').classList.remove('active');
-            // $("body").addClass("no_scroll");
+        if (!this.classList.contains('has-dropdown')) {
+            //	document.getElementById('menu-button').classList.remove('active');
+           // document.getElementById('header').classList.remove('active');
+
 
         }
     }
@@ -30,15 +31,18 @@ for (let i = 0; i < menuLinks.length; i++) {
 // submenu show`n`hide
 
 // Get all elements with class 'has-child'
-var hasSubmenuElements = document.getElementsByClassName('has-child');
+var hasSubmenuElements = document.getElementsByClassName('has-dropdown');
 
 // Loop over the elements
 for (var i = 0; i < hasSubmenuElements.length; i++) {
     // Add the onclick event listener to each element
     hasSubmenuElements[i].onclick = function() {
-        // Get all elements with class 'submenu'
-        var submenuElements = document.getElementsByClassName('[class*="submenu"]');
-        var submenuElements = document.getElementsByClassName('wp-block-navigation-submenu');
+
+		
+		// Get all elements with class 'submenu'
+        //var submenuElements = document.getElementsByClassName('[class*="submenu"]');
+		var submenuElements = this.getElementsByClassName('submenu');
+        // var submenuElements = document.getElementsByClassName('wp-block-navigation-submenu');
 
         // Loop over the elements
         for (var j = 0; j < submenuElements.length; j++) {
